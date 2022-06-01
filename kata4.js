0cbd5ddcb28fec1980e6074ad5ab20c1eee136d2
matigobbi <matigobbi@gmail.com>
2022-06-02 00:14:33 +0200

    2.6

---
+++ kata4.js
@@ -0,0 +1,28 @@
+// Pair of gloves
+// Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine 
+// the number of pair of gloves you can constitute from the gloves you have in your drawer.
+
+// Given an array describing the color of each glove, return the number of pairs you can constitute, assuming that only gloves of the same color can form pairs.
+
+// Examples:
+// input = ["red", "green", "red", "blue", "blue"]
+// result = 2 (1 red pair + 1 blue pair)
+
+// input = ["red", "red", "red", "red", "red", "red"]
+// result = 3 (3 red pairs)
+
+
+
+
+
+
+function numberOfPairs(gloves)
+{
+  //for loop
+  //take the first one and look for one coincidence
+  //if yes, shift the pair and plus 1
+  //if no continue
+  (for i = 0 ; i < gloves.length ; i++){
+    
+  }
+}
