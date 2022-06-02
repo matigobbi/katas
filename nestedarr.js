18f3df726bf0fd1a0936d0f7e9ee4ac95e4feca3
matigobbi <matigobbi@gmail.com>
2022-06-02 22:11:50 +0200

    2.6

---
+++ nestedarr.js
@@ -0,0 +1,17 @@
+
+
+// Modify function multiplyAll so that it returns the product of all the numbers in the sub-arrays of arr.
+
+
+function multiplyAll(arr) {
+  let product = 1;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    product = product * arr[i][j]
+  }
+}
+console.log(product)
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
