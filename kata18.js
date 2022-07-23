f8c1c570aced6fb371e33df3bf686e8382e34b88
matigobbi <matigobbi@gmail.com>
2022-07-23 12:57:32 +0200

    23.07

---
+++ kata18.js
@@ -0,0 +1,26 @@
+// Check to see if a string has the same amount of 'x's and 'o's. 
+// The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+
+function XO(str) {
+  //turn into an arr
+let arr = str.split("");
+  //create  counters for x and o 
+let x=0; 
+let o=0;
+  //loop trougth arr and add count
+for(let i = 0; i < arr.length; i++){
+  if(arr[i]==="x"||arr[i]==="X") x+= 1;
+  if(arr[i]==="o"||arr[i]==="O") o+= 1;
+}
+  // compare and return
+if(x == o) {return true}
+else {return false}
