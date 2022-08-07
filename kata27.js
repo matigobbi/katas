2e7043d820995bd706d7e06ef31af805717c5177
matigobbi <matigobbi@gmail.com>
2022-08-07 11:21:39 +0200

    07.08

---
+++ kata27.js
@@ -0,0 +1,12 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+
+const reverseSeq = n => {
+  let result = []
+ for(let i = n; i>0; i--){
+   result.push(i)
+ }
+  return result
+};
